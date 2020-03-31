718ee8f7d33c6feee7337a02504b76a2925f9620
Lauren Birts <birtslauren@gmail.com>
2020-03-31 20:22:05 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -15,7 +15,7 @@ def play(array)
   puts "Please enter a song name or number:"
   input = gets.strip
   array.each.with_index(1) { |song, index|
-  if input == song || input.to_i == index
+  if input == song || input.to_i == index.to_i
     puts "Playing #{song}"
     break
   else
