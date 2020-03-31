97875db98c33633a9b80f785418ec6e507afed26
Lauren Birts <birtslauren@gmail.com>
2020-03-31 21:57:23 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -25,21 +25,9 @@ def play(array)
   input = gets.strip
   if array.include?(input)
     puts "Playing #{input}"
-    if response.to_i.between?(1,songs.length)
- if song.to_i >= 1 && song.to_i <= songs.size #(1..songs.length).include?(song)
   elsif (1..array.length).to_a.include?(input.to_i)
     puts "Playing #{array[input.to_i - 1]}"
   else
     puts "Invalid input, please try again"
   end
-end
-  array.each.with_index(1) { |song, index|
-  if input == song || input.to_i == index.to_i
-    puts "Playing #{song}"
-    break
-  else
-    puts "Invalid input, please try again"
-    break
-  end
-  }
 end
